e4ecac227b805c407824f4686d420b6eef784a39
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-15 16:30:08 -0300

    Aula 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './todo/create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'todo/create',
+    component: CreateComponent
+  },
+];
+//criacao -> /create
+// edicao -> /edit
+// listagem -> list
+// visualizacao -> /{id}
+//compartilhamento /{id}/shared
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
