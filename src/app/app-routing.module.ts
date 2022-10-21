c077647aae3993da095beb9278627473342e987f
Rafael Moura <rafael.g.moura@hotmail.com>
2022-10-21 19:50:37 -0300

    semifeat: meio aula

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './todo/create/create.component';
+import { ListComponent } from './todo/list/list.component';
 
 const routes: Routes = [
   {
     path:'todo/create',
     component: CreateComponent
   },
+  {
+    path:'todo/list',
+    component:ListComponent
+  }
 ];
 //criacao -> /create
 // edicao -> /edit
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,19 +1,73 @@
 import { Injectable } from '@angular/core';
 import { TodoModel } from '../todo/models/todo.model';
 import * as uuid from 'uuid';
+import { TodoStatus } from '../todo/enums/status.enum';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  constructor() {}
 
-  constructor() { }
-
-  cadastrar(todo: TodoModel):void{
+  cadastrar(todo: TodoModel): void {
+    let todos = this.listaTodos();
     todo.id = uuid.v4();
 
-
+    todos.push(todo);
     localStorage['todos'] = JSON.stringify(todo);
-    console.log('[todos]',localStorage['todos']);
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    console.log('[todos]', localStorage['todos']);
+  }
+
+  atualizar(todo: TodoModel) {
+    let todos = this.listaTodos();
+
+    todos.forEach((todo) => console.log(JSON.stringify(todo)));
+    todos.forEach((t, index, todos) => {
+      if (todo.id === todo.id) {
+        todos[index] = todo;
+      }
+    });
+
+    localStorage.setItem('number', JSON.stringify(todos));
+  }
+
+  listaTodos(): TodoModel[] {
+    return JSON.parse(localStorage['todos']) ?? []; //Checa se o localStorage todos existe, e faz um retorno de acordo
+  }
+
+  remover(id: string): void {
+    let todos = this.listaTodos();
+    todos.filter((todo) => todo.id !== id);
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }
+
+  alteraStatus(id: string, status: TodoStatus) {
+    const todos = this.listaTodos();
+
+    // todos.forEach((todo, index, todos) => {
+    //   if (id === todo.id) {
+    //     todos[index].status = status;
+
+    //     if (status === TodoStatus.CONCLUIDO) {
+    //       todos[index].dataFinalizacao = new Date();
+    //     }
+    //   }
+    // });
+
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].id == id) {
+        todos[i].status = status;
+        if (status == TodoStatus.CONCLUIDO) {
+          todos[i].dataFinalizacao = new Date();
+        }
+
+        break;
+      }
+    }
+
+    localStorage.setItem('todos', JSON.stringify(todos));
   }
 }
